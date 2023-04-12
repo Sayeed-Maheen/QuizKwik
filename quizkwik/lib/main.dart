fb74a37fc7c92ca3bdc7e63c108a344c41445496
sayeed1124 <sayeedhassan1124@gmail.com>
2023-04-11 19:45:39 -0700

    11.04.23

--- quizkwik/lib/main.dart
+++ quizkwik/lib/main.dart
@@ -7,67 +7,65 @@ import 'package:quizkwik/widgets/appColors.dart';
 import 'package:quizkwik/widgets/customSwatch.dart';
 
 void main() {
-  // The entry point of the application. This function runs the application
-  // by creating an instance of MyApp and passing it to runApp() function.
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateStatusBarColor();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // The build method of the root widget of the application. It returns
-    // a ScreenUtilInit widget that provides a screen size adaptation solution
-    // for the application. Inside it, there is a StatusBarWrapper widget that
-    // sets the color and brightness of the status bar, and a GetMaterialApp
-    // widget that is the root of the application widget tree.
     return ScreenUtilInit(
-      designSize: const Size(360, 690), // The design size of the screen
-      minTextAdapt: true, // Whether to adapt text size or not
-      splitScreenMode: true, // Whether to support split screen mode or not
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
       builder: (context, child) {
-        return StatusBarWrapper(
-          child: GetMaterialApp(
-            debugShowCheckedModeBanner:
-                false, // Whether to show the debug banner or not
-            theme: ThemeData(
-              primarySwatch: createMaterialColor(AppColors
-                  .colorPrimary), // The primary color of the application
-              fontFamily:
-                  "Barlow", // The default font family of the application
-            ),
-            initialRoute: RoutesClass
-                .getSplashScreenRoute(), // The initial route of the application
-            getPages:
-                RoutesClass.routes, // The list of routes of the application
-            routes: const {
-              // Your custom routes can be added here
-            },
-          ),
-        );
+        return FutureBuilder(
+            future: _updateStatusBarColor(),
+            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+              return GetMaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  brightness: _isDarkMode ? Brightness.dark : Brightness.light,
+                  primarySwatch: createMaterialColor(AppColors.colorPrimary),
+                  fontFamily: "Barlow",
+                ),
+                initialRoute: RoutesClass.getSplashScreenRoute(),
+                getPages: RoutesClass.routes,
+                routes: const {},
+              );
+            });
       },
     );
   }
-}
 
-class StatusBarWrapper extends StatelessWidget {
-  final Widget child;
-
-  const StatusBarWrapper({Key? key, required this.child}) : super(key: key);
+  Future<void> _updateStatusBarColor() async {
+    await Future.delayed(
+        Duration.zero); // Wait for the next frame to allow the app to build
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: _isDarkMode ? Brightness.light : Brightness.dark,
+    ));
+  }
 
   @override
-  Widget build(BuildContext context) {
-    // A widget that wraps its child widget and sets the color and brightness
-    // of the status bar based on the brightness of the platform (dark or light).
-    final brightness = MediaQuery.of(context).platformBrightness;
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent, // The color of the status bar
-        statusBarIconBrightness: brightness == Brightness.light
-            ? Brightness.dark
-            : Brightness.light, // The brightness of the status bar icons
-        statusBarBrightness: brightness, // The brightness of the status bar
-      ),
-      child: child, // The child widget that is wrapped by StatusBarWrapper
-    );
+  void dispose() {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness:
+          Brightness.dark, // Reset status bar text color when the app is closed
+    ));
+    super.dispose();
   }
 }
